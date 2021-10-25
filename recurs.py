782310d5ab2c3ac21e592a382166678b38d0b618
WiseVenom21 <abhinavas430@gmail.com>
2021-10-25 19:22:14 +0530

    Recursive function & more

--- recurs.py
+++ recurs.py
@@ -1,8 +1,18 @@
 def fac(n):
-    if n==1:
+    if n==1 or n==0:
         return 1
     else:
         return n * fac(n-1)
 
 in1=int(input("Enter your number\n"))
 print("Factorial is ", fac(in1))
+
+def fun(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fun(n-1)+fun(n-2)
+
+print("Fun is ", fun(in1))
